830d974e2120ac062674a7ea59d4144ccf42ed8f
Christopher Roth <chrisevanroth@gmail.com>
2020-03-10 23:17:39 -0400

    commit records after downloading

--- download_data.py
+++ download_data.py
@@ -3,23 +3,16 @@ import gzip
 import json
 import sqlite3
 from tqdm import tqdm
+from multiprocessing import Pool
 
-base_url = "https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/2020-03-01/"
-response = urllib.request.urlopen(base_url + "manifest.txt")
-manifest = response.read().decode('utf-8')
-files = manifest.split("\n")
-files = files[:-2] # last two files are sample and readme
-
-conn = sqlite3.connect("ontovec.db")
-c = conn.cursor()
-c.execute('''CREATE TABLE papers (id text, title text, abstract text, doi text)''')
+def createPaperTable():
+    cur.execute('''CREATE TABLE papers (id text, title text, abstract text, doi text)''')
 
 def insertPaper(cursor, paper):
     sql = "INSERT INTO papers (id,title,abstract,doi) VALUES (?, ?, ?, ?)"
     cursor.execute(sql, (paper['id'], paper['title'], paper['paperAbstract'], paper['doi']))
-
-for file in files:
-    print(file)
+    
+def downloadFile(file, cur):
     response = urllib.request.urlopen(base_url + file)
     unzippedBytes = gzip.decompress(response.read())
     decodedFile = unzippedBytes.decode('utf-8')
@@ -30,14 +23,25 @@ for file in files:
             if jsonPaper and jsonPaper.strip():
                 try:
                     paper = json.loads(jsonPaper)
-                    insertPaper(c, paper)
+                    insertPaper(cur, paper)
                 except:
                     print("failed: {}".format(jsonPaper))
             pbar.update(1)
+            
+dbName = "ontovec.db"
+base_url = "https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/2020-03-01/"
+response = urllib.request.urlopen(base_url + "manifest.txt")
+manifest = response.read().decode('utf-8')
+files = manifest.split("\n")
+files = files[:-2] # last two files are sample and readme
 
-# Save (commit) the changes
+conn = sqlite3.connect(dbName)
+cur = conn.cursor()
+    
+createPaperTable()
+
+for file in files:
+    downloadFile(file, cur)
+    
 conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
 conn.close()
